27d3b9743e4bc75b5055f319cdadb39e201892f1
JhonyCosmo <jhonycosmo@gmail.com>
2013-05-20 21:14:37 -0300

    comit inicial

---
+++ Visao/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Private Sub Button1_MouseClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button1.MouseClick
+        Dim pess As New Cliente
+
+    End Sub
+End Class
